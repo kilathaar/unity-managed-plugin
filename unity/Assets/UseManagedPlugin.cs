b03f13be295cf477a9aef7985f17e069deb1eb80
Jimmy Ljungberg <jimmy.ljungberg@gmail.com>
2020-10-20 19:19:52 +0200

    Anropa metod i Entitet.dll

---
+++ unity/Assets/UseManagedPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Entitet;
+
+public class UseManagedPlugin : MonoBehaviour
+{
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text text = new Text("Hello World");
+        Debug.Log(text.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
